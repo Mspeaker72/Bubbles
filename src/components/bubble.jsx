4de5139df37e7d2cd0c7e782c2797ec38758185a
Lungelo Hlatshwayo <ianhlatshwayo@gmail.com>
2024-05-29 15:03:18 +0200

    test

--- src/components/bubble.jsx
+++ src/components/bubble.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Bubble({initial_text,text,color,className}){
 
@@ -6,10 +6,29 @@ function Bubble({initial_text,text,color,className}){
 
     function handleClick(){
         setText(prevText=> prevText===initial_text ? text:initial_text)
+       
     }
 
+    useEffect(()=>speak,[text_])
+
+    function speak(){
+        
+
+        if(text_===initial_text){
+            const utterance = new SpeechSynthesisUtterance(text)
+            window.speechSynthesis.speak(utterance);
+            return
+        }
+        const utterance = new SpeechSynthesisUtterance(initial_text)
+            window.speechSynthesis.speak(utterance);
+            return
+       
+    }
     return(
+    
         <p onClick={()=>handleClick(text_)} style={{backgroundColor: color}} className={className}>{text_}</p>
+      
+        
     );
 
 }
